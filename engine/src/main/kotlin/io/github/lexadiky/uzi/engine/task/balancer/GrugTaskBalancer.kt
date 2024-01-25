b5fec2854662f67a84eeed7930be79ee5124cdcf
Aleksey Yakovlev <alexey.yakovlef@gmail.com>
2024-01-25 20:37:00 +0700

    improved grug balancer

--- engine/src/main/kotlin/io/github/lexadiky/uzi/engine/task/balancer/GrugTaskBalancer.kt
+++ engine/src/main/kotlin/io/github/lexadiky/uzi/engine/task/balancer/GrugTaskBalancer.kt
@@ -1,17 +1,47 @@
 package io.github.lexadiky.uzi.engine.task.balancer
 
+import io.github.lexadiky.uzi.engine.task.HttpCallTask
+import io.github.lexadiky.uzi.engine.task.NoopTask
 import io.github.lexadiky.uzi.engine.task.ParallelTask
+import io.github.lexadiky.uzi.engine.task.RepeatedTimerTask
 import io.github.lexadiky.uzi.engine.task.UziTask
 
 /**
- * Basic implementation that just splits tasks by explicitly declared parallelism.
+ * Default [TaskBalancer]
  */
 internal class GrugTaskBalancer : TaskBalancer {
+    private val frequencyTaskBalancer = FrequencyTaskBalancer()
 
     override fun balance(task: UziTask, partitions: Int): List<UziTask> {
-        if (task is ParallelTask) {
-            return task.children
+        var previousStep: List<UziTask> = listOf(task)
+        var balancingTokens = partitions.coerceAtLeast(1) // to prevent too many looping
+
+        while (balancingTokens > 0) {
+            val balanced = previousStep.flatMap { balanceImpl(task, partitions) }
+            if (balanced == previousStep || balanced.size >= partitions) {
+                return balanced
+            }
+            previousStep = balanced
+            balancingTokens -= 1
         }
+
+        return previousStep
+    }
+
+
+    private fun balanceImpl(task: UziTask, partitions: Int): List<UziTask> = when (task) {
+        is HttpCallTask -> noopBalance(task)
+        NoopTask -> noopBalance(task)
+        is ParallelTask -> parallelTaskBalance(task)
+            .flatMap { balanceImpl(it, partitions) }
+        is RepeatedTimerTask -> frequencyTaskBalancer.balance(task, partitions)
+    }
+
+    private fun noopBalance(task: UziTask): List<UziTask> {
         return listOf(task)
+    }
+
+    private fun parallelTaskBalance(task: ParallelTask): List<UziTask> {
+        return task.children
     }
 }
