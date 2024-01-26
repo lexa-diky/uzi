1e746c55edf18c9975d9f7d397d686457a7a5859
Aleksey Yakovlev <alexey.yakovlef@gmail.com>
2024-01-26 07:48:03 +0700

    added HttpExecutionPlan

---
+++ engine/src/main/kotlin/io/github/lexadiky/uzi/engine/execution/http/HttpExecutionPlan.kt
@@ -0,0 +1,29 @@
+package io.github.lexadiky.uzi.engine.execution.http
+
+import io.github.lexadiky.uzi.engine.execution.ExecutionPlan
+import io.github.lexadiky.uzi.engine.task.HttpCallTask
+import io.ktor.client.HttpClient
+import io.ktor.client.request.headers
+import io.ktor.client.request.request
+import io.ktor.client.request.url
+import io.ktor.http.HttpMethod
+
+class HttpExecutionPlan(
+    private val task: HttpCallTask,
+    private val client: HttpClient
+) : ExecutionPlan {
+
+    override suspend fun execute() {
+        client.request {
+            method = HttpMethod(task.method)
+            url(task.uri.toURL())
+            headers {
+                task.headers.forEach { (key, values) ->
+                    values.forEach { value ->
+                        append(key, value)
+                    }
+                }
+            }
+        }
+    }
+}
